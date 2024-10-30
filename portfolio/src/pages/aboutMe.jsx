3698a59119a383cd00845c1f8ad17138081234d4
HP USER <Destmil.co@gmail.com>
2024-10-30 14:31:41 -0400

    added code to about me and portfolio pages

---
+++ portfolio/src/pages/aboutMe.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const AboutMe = () => (
+    <section>
+      <img src="portfolio/src/assets/IMG_4854.jpg" alt="Destinee" />
+      <p>My name, as you've read, is Destinee Miles. I was born and raised south of Chicago where i grew up and developed an interest in puzzles and solving. I enjoy real world interactions as a person, but there is so much life in the technical world. 
+      Being a developer brings so many questions i cant wait to answer and keep answering.</p>
+    </section>
+  );
+  
+  export default AboutMe;
+  
+ 
---
+++ portfolio/src/pages/portfolio.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Project from 'portfolio/src/components/project.jsx';
+  
+  const Portfolio = () => (
+    <section>
+      <h1>My Projects</h1>
+      <Project title="" image="" deployedLink="" repoLink="" />
+      <Project title="" image="" deployedLink="" repoLink="" />
+      
+    </section>
+  );
+  
+export default Portfolio;
+  
