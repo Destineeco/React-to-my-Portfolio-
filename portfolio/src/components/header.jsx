76581dfeea384b6a141686cfffe70f55574cf3ef
HP USER <Destmil.co@gmail.com>
2024-10-30 14:11:21 -0400

    added components and pages. added header and nav code

---
+++ portfolio/src/components/header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Navigation from './nav';
+
+const Header = () => (
+  <header>
+    <h1>Destinee Miles</h1>
+    <Navigation />
+  </header>
+);
+
+export default Header;
---
+++ portfolio/src/components/nav.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Navigation = () => (
+  <nav>
+    <Link to="/">About Me</Link>
+    <Link to="/portfolio">Portfolio</Link>
+    <Link to="/contact">Contact</Link>
+    <Link to="/resume">Resume</Link>
+  </nav>
+);
+
+export default Navigation;
