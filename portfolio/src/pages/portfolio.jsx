c61067a0751b9e93aca71f687c69e6c7b4e0e1ea
HP USER <Destmil.co@gmail.com>
2024-10-30 15:40:05 -0400

    added code and resume to resume file

--- portfolio/src/pages/portfolio.jsx
+++ portfolio/src/pages/portfolio.jsx
@@ -4,17 +4,17 @@ import Project from 'portfolio/src/components/project.jsx';
   const Portfolio = () => (
     <section>
       <h1>My Projects</h1>
-      <Project title="Resume" image="" deployedLink="file:///C:/Users/HPUSE/bootcamp/NU-VIRT-FSF-PT-07-2024-U-LOLC/02-Advanced-CSS/02-Challenge/Develop/index.html" repoLink="https://github.com/Destineeco/Employee-Tracker-" />
+      <Project title="Resume" image="portfolio/src/assets/IMG_6062.jpg" deployedLink="file:///C:/Users/HPUSE/bootcamp/NU-VIRT-FSF-PT-07-2024-U-LOLC/02-Advanced-CSS/02-Challenge/Develop/index.html" repoLink="https://github.com/Destineeco/Employee-Tracker-" />
 
-      <Project title=" JS Employee Payroll Tracker" image="" deployedLink="file:///C:/Users/HPUSE/bootcamp/NU-VIRT-FSF-PT-07-2024-U-LOLC/03-JavaScript/02-Challenge/Develop/index.html" repoLink="" />
+      <Project title=" JS Employee Payroll Tracker" image="portfolio/src/assets/IMG_6061.jpg" deployedLink="file:///C:/Users/HPUSE/bootcamp/NU-VIRT-FSF-PT-07-2024-U-LOLC/03-JavaScript/02-Challenge/Develop/index.html" repoLink="" />
 
-      <Project title="Machine Downtime Tracker" image="" deployedLink="file:///C:/Users/HPUSE/bootcamp/projects/Project-1/index.html" repoLink="https://github.com/apatel62/Project-1" />
+      <Project title="Machine Downtime Tracker" image="portfolio/src/assets/IMG_6060.jpg" deployedLink="file:///C:/Users/HPUSE/bootcamp/projects/Project-1/index.html" repoLink="https://github.com/apatel62/Project-1" />
 
-      <Project title="Node README" image="" deployedLink="" repoLink="https://github.com/Destineeco/node-myREADME" />
+      <Project title="Node README" image="portfolio/src/assets/IMG_6066.PNG" deployedLink="" repoLink="https://github.com/Destineeco/node-myREADME" />
 
-      <Project title="Mood Board" image="" deployedLink="file:///C:/Users/HPUSE/bootcamp/NU-VIRT-FSF-PT-07-2024-U-LOLC/04-Web-APIs/02-Challenge/Develop/index.html" repoLink="" />
+      <Project title="Mood Board" image="portfolio/src/assets/IMG_6059.jpg" deployedLink="file:///C:/Users/HPUSE/bootcamp/NU-VIRT-FSF-PT-07-2024-U-LOLC/04-Web-APIs/02-Challenge/Develop/index.html" repoLink="" />
 
-      <Project title="employee traker" image="" deployedLink="" repoLink="https://github.com/Destineeco/Employee-Tracker-" />
+      <Project title="employee traker" image="portfolio/src/assets/IMG_6065.PNG" deployedLink="" repoLink="https://github.com/Destineeco/Employee-Tracker-" />
       
     </section>
   );
---
+++ portfolio/src/pages/resume.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Resume = () => (
+  <section>
+    <h1>Resume</h1>
+    <a href="portfolio/src/assets/_Resume.pdf" download>Download Resume</a>
+    <ul>
+      <li>HTML/CSS</li>
+      <li>JAVASCRIPT</li>
+      <li>TYPESCRIPT</li>
+    </ul>
+  </section>
+);
+
+export default Resume;
